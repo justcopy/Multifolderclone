0bca126446a0e5b9bffcfd90c6afddc7a3e40e6e
Mz49 <52611335+Mz49@users.noreply.github.com>
2019-08-08 23:18:21 -0700

    specify drive id/accounts/folder, removed prompt

--- masshare.py
+++ masshare.py
@@ -1,8 +1,16 @@
 from oauth2client.service_account import ServiceAccountCredentials
-import googleapiclient.discovery, json, progress.bar, glob, sys, time
+import googleapiclient.discovery, json, progress.bar, glob, sys, argparse
 
-stt = time.time()
-contrs = glob.glob('controller/*.json')
+parse = argparse.ArgumentParser(description='A tool to add service accounts to a shared drive from a folder containing credential files.')
+parse.add_argument('--path','-p',default='accounts',help='Specify an alternative path to the service accounts folder.')
+parse.add_argument('--controller','-c',default='controller/*.json',help='Specify the relative path for the controller file.')
+parsereq = parse.add_argument_group('required arguments')
+parsereq.add_argument('--drive-id','-d',help='The ID of the Shared Drive.',required=True)
+
+args = parse.parse_args()
+acc_dir = args.path
+did = args.drive_id
+contrs = glob.glob(args.controller)
 
 try:
 	open(contrs[0],'r')
@@ -17,13 +25,8 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(contrs[0], scopes
 	"https://www.googleapis.com/auth/drive"
 ])
 
-try:
-	did = sys.argv[1]
-except:
-	did = input('Drive ID? ').strip()
-
 drive = googleapiclient.discovery.build("drive", "v3", credentials=credentials)
-aa = glob.glob('accounts/*.json')
+aa = glob.glob('%s/*.json' % acc_dir)
 pbar = progress.bar.Bar("Adding to %s" % did,max=len(aa))
 for i in aa:
 	ce = json.loads(open(i,'r').read())['client_email']
@@ -36,6 +39,3 @@ for i in aa:
 pbar.finish()
 
 print('Complete. You can now drop the controller inside the accounts folder for an added SA.')
-hours, rem = divmod((time.time() - stt),3600)
-minutes, sec = divmod(rem,60)
-print("Elapsed Time:\n{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),sec))
